ca78845d8b128b5ce6a4c22f48ca45240d79ef6a
LionelBeato <lionel.beato@gmail.com>
2020-11-10 16:06:16 -0500

    add test

--- src/test/java/com/wozu/Api2/Api2ApplicationTests.java
+++ src/test/java/com/wozu/Api2/Api2ApplicationTests.java
@@ -1,10 +1,15 @@
 package com.wozu.Api2;
 
+import com.wozu.Api2.Controllers.DogController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Api2ApplicationTests {
+
+	@Autowired
+	DogController dogController;
 
 	@Test
 	void contextLoads() {
